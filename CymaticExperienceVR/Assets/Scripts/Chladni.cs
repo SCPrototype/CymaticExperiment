735482aa562efea954228a471f8930b45db3ebb9
Tiemen Kamp <t.kamp@sintlucasedu.nl>
2019-05-08 17:32:26 +0200

    Implemented Cymatic Experiment with bad FPS.

--- CymaticExperienceVR/Assets/Scripts/Chladni.cs
+++ CymaticExperienceVR/Assets/Scripts/Chladni.cs
@@ -4,57 +4,191 @@ using UnityEngine;
 
 public class Chladni : MonoBehaviour
 {
-
     //Changable variations
-    double start = 0.4;         // a value for start simulation;
-    double wfMax = 7.0;         // a value for end up the simulation;
-    double A = 0.5;             // for Wave attenuation, should between 0 to 1;
-    double k = 0.0;             // for Wave source shift from plate center
+    public static int plateSize = 50;
+
+    public GameObject TargetPlane;
+    public GameObject PlatePixel;
+
+    float start = 0.4f;         // a value for start simulation;
+    float wfMax = 7.0f;         // a value for end up the simulation;
+    float A = 0.5f;             // for Wave attenuation, should between 0 to 1;
+    float k = 0.0f;             // for Wave source shift from plate center
     bool three_d = false;       // 2D/3D draw option;
 
     // unchangable variations
-    ArrayList p;
+    GameObject[,] pixelGrid = new GameObject[plateSize, plateSize];
+    List<Pixel> p;
+    //ArrayList p;
     int R;
-    double lamda;
-    double waveLengthFactor;
-    double waveIncrease = 1.0;
+    float waveLengthFactor;
+    float waveIncrease = 0.01f;
     bool photo = false;
-    double sumOfWholePlate0, sumOfWholePlate1 = 0.0, sumOfWholePlate2;
-    double maxZ = 0;
-    double sum = 0;
+    float sumOfWholePlate0, sumOfWholePlate1 = 0.0f, sumOfWholePlate2;
+    float maxY = 0;
+    float sum = 0;
     int frameNr = 1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        //size(251, 251, P2D);
-        sumOfWholePlate0 = sumOfWholePlate2 = width * height;
-        R = (int)(-2.0 / Math.log10(A)) + 1;
+        sumOfWholePlate0 = sumOfWholePlate2 = plateSize * plateSize;
+        R = (int)(-2.0 / Mathf.Log10(A)) + 1;
         prepare();
-        smooth();
     }
 
     void prepare()
     {
+        float pixelSizeX = TargetPlane.transform.localScale.x / plateSize;
+        float pixelSizeZ = TargetPlane.transform.localScale.z / plateSize;
+        for (int i = 0; i < plateSize; i++)
+        {
+            for (int j = 0; j < plateSize; j++)
+            {
 
+                pixelGrid[i, j] = GameObject.Instantiate(PlatePixel, TargetPlane.transform);
+                pixelGrid[i, j].transform.localScale = new Vector3(pixelSizeX, 0.1f, pixelSizeZ);
+                pixelGrid[i, j].transform.position = TargetPlane.transform.position + new Vector3(pixelSizeX * (i - (plateSize / 2)) * 10, 0.05f, pixelSizeZ * (j - (plateSize / 2)) * 10);
+            }
+        }
+
+        p = new List<Pixel>();
+        for (int i = 0; i < Mathf.Floor(plateSize / 2.0f); i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Pixel pp = new Pixel(j, i, R, A, k);
+                float lamda = plateSize / (start + waveLengthFactor);
+                pp.interference(lamda);
+                p.Add(pp);
+            }
+        }
     }
 
     void doInterference()
     {
-
+        sum = 0.0f;
+        maxY = 0.0f;
+        float lamda = plateSize / (start + waveLengthFactor);
+        for (int i = 0; i < p.Count; i++)
+        {
+            
+            //Pixel pp = (Pixel)p[i];
+            p[i].interference(lamda);
+            sum += Mathf.Abs(p[i].getY());
+            maxY = Mathf.Abs(p[i].getY()) > maxY ? Mathf.Abs(p[i].getY()) : maxY;
+        }
     }
 
     void draw()
     {
+        //Debug.Log("frameNr=" + frameNr + "  R=" + R + "  waveLengthFactor=" + waveLengthFactor);
 
+        waveLengthFactor = Mathf.Ceil(frameNr * waveIncrease * 100) / 100;
+        doInterference();
+
+        float y = 255.0f / maxY;
+
+        Color pixelClr = new Color(0, 0, 0);
+
+        for (int i = 0; i < p.Count; i++)
+        {
+            Pixel pp = p[i];
+
+            float clr = (y * Mathf.Abs(pp.getY())) / 255.0f;
+
+
+            if (photo)
+            {
+                pixelClr = new Color(clr, clr, clr);
+            }
+            else
+            {
+                if (pp.getY() > 0)
+                {
+                    pixelClr = new Color(clr, 0, 0);
+                }
+                if (pp.getY() < 0)
+                {
+                    pixelClr = new Color(0, 0, clr);
+                }
+            }
+
+            if (three_d)
+            {
+                float fy = (float)(0.1 * pp.getY() * y);
+                pixelGrid[pp.getX() + (plateSize/2), pp.getZ() + (plateSize / 2)].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getX(), pp.getZ() + (plateSize / 2)].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getX(), pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getX() + (plateSize / 2), pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getZ() + (plateSize / 2), pp.getX() + (plateSize / 2)].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getZ(), pp.getX() + (plateSize / 2)].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getZ(), pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[pp.getZ() + (plateSize / 2), pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                //point(pp.getX(), pp.getY(), fz);
+                //point(-pp.getX(), pp.getY(), fz);
+                //point(-pp.getX(), -pp.getY(), fz);
+                //point(pp.getX(), -pp.getY(), fz);
+                //point(pp.getY(), pp.getX(), fz);
+                //point(-pp.getY(), pp.getX(), fz);
+                //point(-pp.getY(), -pp.getX(), fz);
+                //point(pp.getY(), -pp.getX(), fz);
+            }
+            else
+            {
+                pixelGrid[(plateSize / 2) + pp.getX(), (plateSize / 2) + pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) - pp.getX(), (plateSize / 2) + pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) - pp.getX(), (plateSize / 2) - pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) + pp.getX(), (plateSize / 2) - pp.getZ()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) + pp.getZ(), (plateSize / 2) + pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) - pp.getZ(), (plateSize / 2) + pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) - pp.getZ(), (plateSize / 2) - pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+                pixelGrid[(plateSize / 2) + pp.getZ(), (plateSize / 2) - pp.getX()].GetComponent<MeshRenderer>().material.color = pixelClr;
+
+                //point(pp.getX(), pp.getY());
+                //point(-pp.getX(), pp.getY());
+                //point(-pp.getX(), -pp.getY());
+                //point(pp.getX(), -pp.getY());
+                //point(pp.getY(), pp.getX());
+                //point(-pp.getY(), pp.getX());
+                //point(-pp.getY(), -pp.getX());
+                //point(pp.getY(), -pp.getX());
+            }
+        }
+
+        if (frameNr > Mathf.Floor(wfMax / waveIncrease))
+        {
+            Application.Quit();
+        }
+
+        if (photo)
+        {
+            //saveFrame("Exp2d/"+Math.floor(A*100)/100+"_"+Math.floor(k*100)/100+"/photo/"+frameNr+"-"+waveLengthFactor+".png");
+            photo = false;
+            frameNr++;
+        }
+        //saveFrame("Exp2d/"+Math.floor(A*100)/100+"_"+Math.floor(k*100)/100+"/movie/"+frameNr+"-"+waveLengthFactor+".png");
+
+        if (!photo)
+        {
+            sumOfWholePlate2 = sum;
+            if (sumOfWholePlate0 < sumOfWholePlate1 && sumOfWholePlate1 > sumOfWholePlate2 && !photo)
+            {
+                //photo = true;
+                //frameNr--;
+            }
+
+            sumOfWholePlate0 = sumOfWholePlate1;
+            sumOfWholePlate1 = sumOfWholePlate2;
+            frameNr++;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        draw();
     }
 }
 
--- CymaticExperienceVR/Assets/Scripts/Pixel.cs
+++ CymaticExperienceVR/Assets/Scripts/Pixel.cs
@@ -2,12 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Pixel
 {
+    int x, z;
+    float y;
+    int m;
+    float[] a;
+    float[] distances;
+
+    public Pixel(int x, int z, int m, float A, float k)
+    {
+        this.x = x;
+        this.z = z;
+        this.m = m;
+        int nrDist = 2 * m * m + 2 * m + 1;
+        this.distances = new float[nrDist];
+        this.a = new float[nrDist];
+        calculateDistance(A, k);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void calculateDistance(float A, float kk)
+    {
+        int k = 0;
+        for (int i = 0; i <= this.m; i++)
+        {
+            for (int j = i; j >= i - Mathf.Floor(i / 2); j--)
+            {
+                float w1 = j * Chladni.plateSize;
+                float h1 = (i - j) * Chladni.plateSize;
+                float w2 = (i - j) * Chladni.plateSize;
+                float h2 = j * Chladni.plateSize;
+                if (i == 0)
+                {
+                    this.distances[k++] = Mathf.Sqrt(this.x * this.x + this.z * this.z) - kk;
+                }
+                else if (j == i)
+                {
+                    this.distances[k++] = Mathf.Sqrt((w1 + this.x) * (w1 + this.x) + (this.z) * (this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((w1 - this.x) * (w1 - this.x) + (this.z) * (this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((this.x) * (this.x) + (h2 + this.z) * (h2 + this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((this.x) * (this.x) + (h2 - this.z) * (h2 - this.z)) - kk;
+                }
+                else
+                {
+                    this.distances[k++] = Mathf.Sqrt((w1 + this.x) * (w1 + this.x) + (h1 + this.z) * (h1 + this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((w1 - this.x) * (w1 - this.x) + (h1 + this.z) * (h1 + this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((w1 + this.x) * (w1 + this.x) + (h1 - this.z) * (h1 - this.z)) - kk;
+                    this.distances[k++] = Mathf.Sqrt((w1 - this.x) * (w1 - this.x) + (h1 - this.z) * (h1 - this.z)) - kk;
+                    if (j != i - j)
+                    {
+                        this.distances[k++] = Mathf.Sqrt((w2 + this.x) * (w2 + this.x) + (h2 + this.z) * (h2 + this.z)) - kk;
+                        this.distances[k++] = Mathf.Sqrt((w2 - this.x) * (w2 - this.x) + (h2 + this.z) * (h2 + this.z)) - kk;
+                        this.distances[k++] = Mathf.Sqrt((w2 + this.x) * (w2 + this.x) + (h2 - this.z) * (h2 - this.z)) - kk;
+                        this.distances[k++] = Mathf.Sqrt((w2 - this.x) * (w2 - this.x) + (h2 - this.z) * (h2 - this.z)) - kk;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < this.distances.Length; i++) this.a[i] = Mathf.Pow(A, this.distances[i] / Chladni.plateSize);
+    }
+
+    public float interference(float lamda)
+    {
+        this.y = 0.0f;
+        for (int i = 0; i < this.distances.Length; i++)
+        {
+            this.y += this.a[i] * Mathf.Sin(2.0f * Mathf.PI * (this.distances[i] - Mathf.Floor(this.distances[i] / lamda) * lamda) / lamda);
+        }
+        return this.y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+    public float getY()
+    {
+        return y;
+    }
+    public int getZ()
+    {
+        return z;
     }
 
     // Update is called once per frame
@@ -16,79 +98,3 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 }
-
-/*
-
-class Pixel
-{
-  int x, y;
-  double z;
-  int m;
-  double[] a;
-  double[] distances;
-  
-  Pixel(int x, int y, int m, double A, double k)
-  {
-    this.x = x;
-    this.y = y;
-    this.m = m;
-    int nrDist = 2*m*m + 2*m + 1;
-    this.distances = new double[nrDist];
-    this.a = new double[nrDist];
-    calculateDistance(A, k);
-  }
-  
-  void calculateDistance(double A, double kk)
-  {
-    int k = 0;
-    for(int i = 0; i <= this.m; i++)
-    {
-      for (int j = i; j >= i-floor(i/2); j--)
-      {
-        double w1 = j*width;
-        double h1 = (i-j)*height;
-        double w2 = (i-j)*width;
-        double h2 = j*height;
-        if (i == 0)
-        {
-          this.distances[k++] = Math.sqrt(this.x*this.x+this.y*this.y) - kk;
-        }
-        else if (j == i)
-        {
-          this.distances[k++] = Math.sqrt((w1+this.x)*(w1+this.x) + (this.y)*(this.y)) - kk;
-          this.distances[k++] = Math.sqrt((w1-this.x)*(w1-this.x) + (this.y)*(this.y)) - kk;
-          this.distances[k++] = Math.sqrt((this.x)*(this.x) + (h2+this.y)*(h2+this.y)) - kk;
-          this.distances[k++] = Math.sqrt((this.x)*(this.x) + (h2-this.y)*(h2-this.y)) - kk;
-        }  
-        else
-        {
-          this.distances[k++] = Math.sqrt((w1+this.x)*(w1+this.x) + (h1+this.y)*(h1+this.y)) - kk;
-          this.distances[k++] = Math.sqrt((w1-this.x)*(w1-this.x) + (h1+this.y)*(h1+this.y)) - kk;
-          this.distances[k++] = Math.sqrt((w1+this.x)*(w1+this.x) + (h1-this.y)*(h1-this.y)) - kk;
-          this.distances[k++] = Math.sqrt((w1-this.x)*(w1-this.x) + (h1-this.y)*(h1-this.y)) - kk;
-          if (j != i-j)
-          {
-            this.distances[k++] = Math.sqrt((w2+this.x)*(w2+this.x) + (h2+this.y)*(h2+this.y)) - kk;
-            this.distances[k++] = Math.sqrt((w2-this.x)*(w2-this.x) + (h2+this.y)*(h2+this.y)) - kk;
-            this.distances[k++] = Math.sqrt((w2+this.x)*(w2+this.x) + (h2-this.y)*(h2-this.y)) - kk;
-            this.distances[k++] = Math.sqrt((w2-this.x)*(w2-this.x) + (h2-this.y)*(h2-this.y)) - kk;
-          }
-        }
-      }
-    }
-    
-    for(int i = 0; i < this.distances.length; i++) this.a[i] = Math.pow(A, this.distances[i]/width);
-  }
-  
-  double interference(double lamda)
-  {
-    this.z = 0.0;
-    for (int i = 0; i < this.distances.length; i++)
-    {
-      this.z += this.a[i]*Math.sin(2.0*Math.PI*(this.distances[i] - Math.floor(this.distances[i] / lamda) * lamda) / lamda);
-    }
-    return this.z;
-  }
-}
-
-*/
