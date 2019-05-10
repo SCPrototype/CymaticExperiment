4daa5bddd267778a74a61df67dc8d5cd4bf8f1f0
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2019-05-10 16:46:15 +0200

    Tweaking.

---
+++ CymaticExperienceVR/Assets/Scripts/SpawningButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SpawningButton : MonoBehaviour
+{
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject spawningPoint;
+    public GameObject sandBucket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnBucket()
+    {
+        //controllerEvents.buttonOnePressed += VRTK_ControllerEvents
+        Debug.Log("Spawning bucket");
+        Instantiate(sandBucket, this.transform.position, this.transform.rotation);
+    }
+}
