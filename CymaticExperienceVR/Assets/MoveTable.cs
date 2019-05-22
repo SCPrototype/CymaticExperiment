c730f16997639a3e57fdcc4eebfa5cafcee3faf7
PieterSchreurs <pieter_j_schreurs@hotmail.com>
2019-05-22 15:01:28 +0200

    Commit before merge.

--- CymaticExperienceVR/Assets/MoveTable.cs
+++ CymaticExperienceVR/Assets/MoveTable.cs
@@ -6,7 +6,7 @@ public class MoveTable : MonoBehaviour
 {
     public GameObject tableController;
     public GameObject tableObject;
-    public GameObject tableHeight;
+    public GameObject tableController2;
     private bool positionHasChanged = false;
     // Start is called before the first frame update
     void Start()
@@ -19,17 +19,18 @@ public class MoveTable : MonoBehaviour
     {
         if (positionHasChanged)
         {
-            //Easy way, dragging the Y of the full table.
+            if(tableController.transform.childCount != 0)
+            {
+                //Left one is selected.
+                tableController2.transform.position = new Vector3(tableController2.transform.position.x, tableController.transform.position.y, tableController2.transform.position.z);
+            } else
+            {
+                //Right one is selected.
+                tableController.transform.position = new Vector3(tableController.transform.position.x, tableController2.transform.position.y, tableController.transform.position.z);
+            }
             tableObject.transform.position = new Vector3(tableObject.transform.position.x, tableController.transform.position.y, tableObject.transform.position.z);
-            positionHasChanged = false;
-            /*
-            You use AnimationState.time to jump to a specific time in an animation.
-            animation["MyAnimation"].time = 5.0;
-            Idea jelmer had:
-            Get value from ChanggeTablePosition(int)
-            Then use that INT to jump to a specific frame within the animation. 
-            Might get some complications if there's some frames which don't line up in height in the animation.
-            */
+           
+            positionHasChanged = false;    
         }
     }
 
