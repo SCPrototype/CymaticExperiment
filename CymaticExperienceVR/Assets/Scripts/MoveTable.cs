16752e38ec3ba94673e34387981cded41c33fff4
PieterSchreurs <pieter_j_schreurs@hotmail.com>
2019-05-29 10:42:06 +0200

    `pull prepare.

--- CymaticExperienceVR/Assets/Scripts/MoveTable.cs
+++ CymaticExperienceVR/Assets/Scripts/MoveTable.cs
@@ -6,7 +6,7 @@ public class MoveTable : MonoBehaviour
 {
     public GameObject tableController;
     public GameObject tableObject;
-    private float _startingPOS = 0.7112985f;
+    public GameObject tableController2;
     private bool positionHasChanged = false;
     // Start is called before the first frame update
     void Start()
@@ -19,8 +19,19 @@ public class MoveTable : MonoBehaviour
     {
         if (positionHasChanged)
         {
-            tableObject.transform.position = new Vector3(-0.108901f, (float)tableController.transform.position.y - _startingPOS, 0.02523238f);
-            positionHasChanged = false;    
+            if (tableController.transform.childCount != 0)
+            {
+                //Left one is selected.
+                tableController2.transform.position = new Vector3(tableController2.transform.position.x, tableController.transform.position.y, tableController2.transform.position.z);
+            }
+            else
+            {
+                //Right one is selected.
+                tableController.transform.position = new Vector3(tableController.transform.position.x, tableController2.transform.position.y, tableController.transform.position.z);
+            }
+            tableObject.transform.position = new Vector3(tableObject.transform.position.x, tableController.transform.position.y, tableObject.transform.position.z);
+
+            positionHasChanged = false;
         }
     }
 
